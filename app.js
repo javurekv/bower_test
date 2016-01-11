87a2dab1e9a1b1c3095b40ffde350f0ceadc0713
unknown <javurek@mail.ru>
2016-01-11 20:21:20 +0200

    First test version

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+'use strict';
+
+/* Controllers */
+var myApp = angular.module('myApp', []);
+
+myApp.controller('allCacegory',['$scope','$http', function($scope, $http) {
+  $http.get('http://dev.viewily.com:8080/core-server-0.0.1-SNAPSHOT/category/all').success(function(data, status, headers, config) {
+      $scope.phones = angular.toJson(data);
+      return data;
+  });
+}]);
+
+
+
 function test () {
-  alert("Hello world!" );
+  alert('asdad');
 }
+
+test();
